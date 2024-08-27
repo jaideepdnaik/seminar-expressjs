b8706e83c535a55d97cd5f7a293e2cf976248b2d
jaideepdnaik <jaideep.d@juegostudioz.com>
2024-08-27 09:53:30 +0530

    Added three new routes to the server: '/', '/home', and '/help', each responding with a simple message.

--- index.js
+++ index.js
@@ -6,3 +6,15 @@ const PORT = 3000; // OR 8080
 app.listen(PORT, () => {
     console.log(`Server is Listening on port http://localhost:${PORT}`);
 });
+
+app.get('/', (req, res) => {
+    res.send('You\'ve reached the root path');
+});
+
+app.get('/home', (req, res) => {
+    res.send('You\'ve reached the home page');
+});
+
+app.get('/help', (req, res) => {
+    res.send('You\'ve reached the help page');
+});
