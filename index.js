6739a423bc20c0419444404ba912ee18b4208280
jaideepdnaik <jaideep.d@juegostudioz.com>
2024-08-27 09:57:53 +0530

    Added GET request for root path, wildcard route, and POST request for root path.

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ app.listen(PORT, () => {
     console.log(`Server is Listening on port http://localhost:${PORT}`);
 });
 
+//GET Request
 app.get('/', (req, res) => {
     res.send('You\'ve reached the root path');
 });
@@ -19,6 +20,13 @@ app.get('/help', (req, res) => {
     res.send('You\'ve reached the help page');
 });
 
+//Wild card route.
 app.get('*', (req, res) => {
     res.send('This path doesn\'t exist.');
 });
+
+//POST request
+app.post('/', (req, res) => {
+    res.send('You\'ve reached the root path by sending a POST request. ');
+});
+
