541ef785ff5c5e302cfb629fb47da1d5f46cd289
jaideepdnaik <jaideep.d@juegostudioz.com>
2024-08-27 11:25:03 +0530

    Added EJS view engine configuration and two new routes for home and about pages"

--- ejs/index.js
+++ ejs/index.js
@@ -1,9 +1,19 @@
 const express = require("express");
 const app = express();
+// const path = require("path");
 
 const PORT = 3000; // OR 8080
 
 app.set("view engine", "ejs");
+// app.set("views", path.join(__dirname, "/views"));
+
+app.get("/", (req, res) => {
+    res.render("home.ejs");
+});
+
+app.get("/about", (req, res) => {
+    res.send("This is the about page.");
+});
 
 app.listen(PORT, () => {
     console.log(`Server is Listening on port http://localhost:${PORT}`);
