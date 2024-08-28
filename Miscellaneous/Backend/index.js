e91a56bb0a5ec4e5a7e8ad282d3b06a39598161f
Jaideep D Naik <jaideepchinnu3@gmail.com>
2024-08-29 00:02:17 +0530

    Added comments

--- Miscellaneous/Backend/index.js
+++ Miscellaneous/Backend/index.js
@@ -6,12 +6,15 @@ const port = 8080;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+//GET Request
 app.get("/register", (req, res) => {
     let { user, password } = req.query;
     res.send(`Standard GET Response. Welcome ${user}!`);
 });
 
+//POST Request
 app.post("/register", (req, res) => {
+    console.log(req.body);
     let { user, password } = req.body;
     res.send(`Standard POST Response. Welcome ${user}!`);
 });
